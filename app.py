60d78196b415b218f40d428a526b2643605053a7
Hsiao Yao <tomtogo1260@gmail.com>
2024-12-04 01:50:15 +0800

    use fiber as a conductor

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-import cv2
 import nrrd
+import random
 import tifffile
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from scipy.ndimage import convolve
-from matplotlib.colors import ListedColormap
-from skimage.morphology import skeletonize
 
-def down_sampling(array, factor, mean=True):
+from skimage.morphology import skeletonize
+from matplotlib.colors import ListedColormap
+from concurrent.futures import ThreadPoolExecutor
+
+def down_sampling(array, rescale, mean=True):
     nz, ny, nx = array.shape
-    tz = (nz // factor) * factor
-    ty = (ny // factor) * factor
-    tx = (nx // factor) * factor
+    tz = (nz // rescale) * rescale
+    ty = (ny // rescale) * rescale
+    tx = (nx // rescale) * rescale
     trimmed_array = array[:tz, :ty, :tx]
     # trimmed_array = array[:, :ty, :tx]
 
     if (mean):
         downscaled = trimmed_array.reshape(
             # nz, 1,
-            tz // factor, factor,
-            ty // factor, factor,
-            tx // factor, factor
+            tz // rescale, rescale,
+            ty // rescale, rescale,
+            tx // rescale, rescale
         ).mean(axis=(1, 3, 5)).astype(array.dtype)
     else:
         downscaled = np.max(
             trimmed_array.reshape(
                 # nz, 1,
-                tz // factor, factor,
-                ty // factor, factor,
-                tx // factor, factor
+                tz // rescale, rescale,
+                ty // rescale, rescale,
+                tx // rescale, rescale
             ),axis=(1, 3, 5)
         ).astype(array.dtype)
 
@@ -58,8 +59,32 @@ def generate_graph(skel):
 
     return G_filtered, components
 
-def update_potential_stack(potential_stack):
-    pc_pad = np.pad(potential_stack, pad_width=1, mode='reflect')
+def generate_mask_stack(skeleton_stack, num_worker=5):
+    # graph
+    d, h, w = skeleton_stack.shape
+    component_list, processing_list = [], []
+
+    def processing(z):
+        print('processing graphs ', z)
+        G, components = generate_graph(skeleton_stack[z, :, :])
+        component_list.append(components)
+
+    with ThreadPoolExecutor(max_workers=num_worker) as executor:
+      futures = [ executor.submit(processing, z) for z in range(d) ]
+
+    # mask
+    mask_stack = np.zeros_like(skeleton_stack, dtype=np.uint8)
+
+    for z in range(d):
+        components = component_list[z][:255]
+        random.shuffle(components)
+        for i, component in enumerate(components, start=1):
+            for node in component: mask_stack[z, node[0], node[1]] = i
+
+    return mask_stack
+
+def update_potential(potential):
+    pc_pad = np.pad(potential, pad_width=1, mode='reflect')
 
     pc  = pc_pad[1:-1, 1:-1,  :-2].copy()
     pc += pc_pad[1:-1, 1:-1,   2:]
@@ -71,66 +96,27 @@ def update_potential_stack(potential_stack):
 
     return pc
 
-def update_potential_level(potential_stack, boundary):
-    counts = np.zeros_like(potential_stack)
-    p_avg = potential_stack.copy()
-    pc = potential_stack.copy()
-    bc = potential_stack.copy()
-    bf = boundary.copy().astype(potential_stack.dtype)
-
-    p_avg[1:-1, 1:-1, 1:-1]  = pc[1:-1, 1:-1,  :-2]
-    p_avg[1:-1, 1:-1, 1:-1] += pc[1:-1, 1:-1,   2:]
-    p_avg[1:-1, 1:-1, 1:-1] += pc[1:-1,  :-2, 1:-1]
-    p_avg[1:-1, 1:-1, 1:-1] += pc[1:-1,   2:, 1:-1]
-    p_avg[1:-1, 1:-1, 1:-1] += pc[ :-2, 1:-1, 1:-1]
-    p_avg[1:-1, 1:-1, 1:-1] += pc[  2:, 1:-1, 1:-1]
-    p_avg[1:-1, 1:-1, 1:-1] /= 6
-
-    p_avg[boundary == 0] = 0
-    p_avg[boundary == 1] -= 100
-    p_avg[boundary == 3] -= 180
-
-    bc[1:-1, 1:-1, 1:-1]  = p_avg[1:-1, 1:-1,  :-2]
-    bc[1:-1, 1:-1, 1:-1] += p_avg[1:-1, 1:-1,   2:]
-    bc[1:-1, 1:-1, 1:-1] += p_avg[1:-1,  :-2, 1:-1]
-    bc[1:-1, 1:-1, 1:-1] += p_avg[1:-1,   2:, 1:-1]
-    bc[1:-1, 1:-1, 1:-1] += p_avg[ :-2, 1:-1, 1:-1]
-    bc[1:-1, 1:-1, 1:-1] += p_avg[  2:, 1:-1, 1:-1]
-
-    counts[1:-1, 1:-1, 1:-1]  = (boundary[1:-1, 1:-1,  :-2] > 0)
-    counts[1:-1, 1:-1, 1:-1] += (boundary[1:-1, 1:-1,  :-2] > 0)
-    counts[1:-1, 1:-1, 1:-1] += (boundary[1:-1,  :-2, 1:-1] > 0)
-    counts[1:-1, 1:-1, 1:-1] += (boundary[1:-1,   2:, 1:-1] > 0)
-    counts[1:-1, 1:-1, 1:-1] += (boundary[ :-2, 1:-1, 1:-1] > 0)
-    counts[1:-1, 1:-1, 1:-1] += (boundary[  2:, 1:-1, 1:-1] > 0)
-
-    nonzero = counts > 0
-    pc[nonzero] += bc[nonzero] / counts[nonzero] * 0.3
-    pc[boundary > 0] = potential_stack[boundary > 0]
-
-    return pc
-
-def update_mask_stack(potential_stack, mask_stack, counts_stack, axis=0):
-    pc = potential_stack.copy()
-    d = potential_stack.shape[axis]
+def update_conductor(potential, conductor, counts, axis=0):
+    pc = potential.copy()
+    d = potential.shape[axis]
 
     for l in range(d):
-        counts = counts_stack[l]
+        c = counts[l]
 
         if (axis == 0):
-            mask = mask_stack[l, :, :]
+            mask = conductor[l, :, :]
             p = pc[l, :, :]
         elif (axis == 1):
-            mask = mask_stack[:, l, :]
+            mask = conductor[:, l, :]
             p = pc[:, l, :]
         else:
-            mask = mask_stack[:, :, l]
+            mask = conductor[:, :, l]
             p = pc[:, :, l]
 
         sums = np.bincount(mask.flatten(), weights=p.flatten(), minlength=256)
         averages = np.zeros_like(sums)
-        nonzero = counts > 0
-        averages[nonzero] = sums[nonzero] / counts[nonzero]
+        nonzero = c > 0
+        averages[nonzero] = sums[nonzero] / c[nonzero]
 
         if (axis == 0):
             pc[l, :, :] = averages[mask]
@@ -141,216 +127,127 @@ def update_mask_stack(potential_stack, mask_stack, counts_stack, axis=0):
 
     return pc
 
-def generate_mask_stack(skeleton_stack):
-    d, h, w = skeleton_stack.shape
-
-    # graph
-    G_list, component_list = [], []
-
-    for z in range(d):
-        print('processing graphs ', z)
-        G, components = generate_graph(skeleton_stack[z, :, :])
-        component_list.append(components)
-        G_list.append(G)
-
-    # mask
-    mask_stack = np.zeros_like(skeleton_stack, dtype=np.uint8)
-
-    for z in range(d):
-        for i, component in enumerate(component_list[z][:255], start=1):
-            for node in component: mask_stack[z, node[0], node[1]] = i
-
-    return mask_stack
-
-def generate_2d_points_array(potential, points_top, points_bottom, num_depth):
-    points_top = np.array(points_top)
-    points_bottom = np.array(points_bottom)
-
-    points_grid = np.linspace(points_top, points_bottom, num_depth * 5, axis=0).astype(int)
-    potential_grid = potential[points_grid[..., 1], points_grid[..., 0]]
-    levels = np.linspace(255, 0, num_depth) 
-
-    # y, x, 2
-    num_points = points_top.shape[0]
-    points_array = np.zeros((num_depth, num_points, 2))
-
-    for i in range(num_points):
-        points_array[:, i, 0] = np.interp(levels, potential_grid[:, i][::-1], points_grid[:, i, 0][::-1])
-        points_array[:, i, 1] = np.interp(levels, potential_grid[:, i][::-1], points_grid[:, i, 1][::-1])
-
-    return points_array
-
 if __name__ == "__main__":
-    z, y, x, factor = 3513, 1900, 3400, 5
-    tif_dir = f'/Users/yao/Desktop/distort-space-test/{z:05}_{y:05}_{x:05}_volume.tif'
-    mask_dir = f'/Users/yao/Desktop/distort-space-test/{z:05}_{y:05}_{x:05}_mask.nrrd'
+    # params & path
+    z, y, x = 3513, 1900, 3400
+
+    rescale, num_worker = 2, 8
+    top_electrode_label, bot_electrode_label = 3, 1
+    top_electrode_level, bot_electrode_level = 180, 100
+
+    volume_dir = f'/Users/yao/Desktop/field-based-visualization/{z:05}_{y:05}_{x:05}_volume.tif'
+    electrode_dir = f'/Users/yao/Desktop/field-based-visualization/{z:05}_{y:05}_{x:05}_mask.nrrd'
+    conductor_dir = f'/Users/yao/Desktop/field-based-visualization/{z:05}_{y:05}_{x:05}_fiber.nrrd'
 
     # plot init
-    fig, axes = plt.subplots(2, 5, figsize=(7, 3))
-
-    axes = axes.ravel()
+    fig, axes = plt.subplots(1, 6, figsize=(15, 5))
     for ax in axes: ax.axis('off')
 
-    # load tif stack
-    tif_stack_origin = tifffile.imread(tif_dir)
-    # tif_stack_origin = tif_stack_origin[:3, :, :]
-    # tif_stack_origin = tif_stack_origin[:2, 140:600, :]
-    tif_stack = down_sampling(tif_stack_origin, factor)
+    # load volume
+    volume = tifffile.imread(volume_dir)
+    volume = down_sampling(volume, rescale)
+    d, h, w = volume.shape
 
-    d, h, w = tif_stack.shape
-    d0, h0, w0 = tif_stack_origin.shape
+    # load electrode
+    electrode, header = nrrd.read(electrode_dir)
+    electrode = np.asarray(electrode)
+    electrode = down_sampling(electrode, rescale, False)
 
-    axes[0].imshow(tif_stack[d//2, :, :], cmap='gray')
-    axes[5].imshow(tif_stack[:, :, w//2], cmap='gray')
+    electrode_temp = np.zeros_like(electrode)
 
-    # load & smooth boundary
-    top_label, bot_label = 3, 1
-
-    boundary, header = nrrd.read(mask_dir)
-    # boundary = boundary[:3, :, :]
-    # boundary = boundary[:2, 140:600, :]
-    boundary = down_sampling(boundary, factor, False)
-    boundary_temp = np.zeros_like(boundary)
-
-    for label in [top_label, bot_label]:
-        print('Processing label:', label)
-        mask_label = (boundary == label).astype(np.uint8)
+    for label in [top_electrode_label, bot_electrode_label]:
+        print('Processing electrode:', label)
+        mask_label = (electrode == label).astype(np.uint8)
 
         for z in range(d):
             skeleton = skeletonize(mask_label[z])
-            boundary_temp[z][skeleton] = label
+            electrode_temp[z][skeleton] = label
 
-        # boundary_temp[boundary == label] = label
+    electrode = electrode_temp
 
-    boundary = boundary_temp
+    axes[0].set_title("Volume")
+    axes[0].imshow(volume[d//2, :, :], cmap='gray')
+    axes[0].contour(electrode[d//2, :, :] * 255, colors='blue', linewidths=0.5)
 
-    axes[0].contour(boundary[d//2, :, :] * 255, colors='blue', linewidths=0.5)
-    axes[5].contour(boundary[:, :, w//2] * 255, colors='blue', linewidths=0.5)
+    # load conductor
+    conductor, header = nrrd.read(conductor_dir)
+    conductor = np.asarray(conductor).astype(bool)
+    conductor = down_sampling(conductor, rescale, False)
 
-    # blur & edge & skeleton
-    blur_stack_z = np.zeros_like(tif_stack)
-    edge_stack_z = np.zeros_like(tif_stack)
-    skeleton_stack_z = np.zeros_like(tif_stack, dtype=bool)
+    for z in range(d): conductor[z] = skeletonize(conductor[z])
 
-    for z in range(d):
-        blur_stack_z[z, :, :] = cv2.GaussianBlur(tif_stack[z, :, :], (7, 7), 0)
-        edge_stack_z[z, :, :] = cv2.Canny(blur_stack_z[z, :, :], threshold1=90, threshold2=100)
-        skeleton_stack_z[z, :, :] = skeletonize(edge_stack_z[z, : , :])
+    axes[1].set_title("Conductor")
+    axes[1].imshow(conductor[d//2, :, :], cmap='gray')
+    axes[1].contour(electrode[d//2, :, :] * 255, colors='blue', linewidths=0.5)
 
-    blur_stack_x = np.zeros_like(tif_stack)
-    edge_stack_x = np.zeros_like(tif_stack)
-    skeleton_stack_x = np.zeros_like(tif_stack, dtype=bool)
+    # conductor along z & x
+    conductor_z = generate_mask_stack(conductor, num_worker)
 
-    for x in range(w):
-        blur_stack_x[:, :, x] = cv2.GaussianBlur(tif_stack[:, :, x], (7, 7), 0)
-        edge_stack_x[:, :, x] = cv2.Canny(blur_stack_x[:, :, x], threshold1=90, threshold2=100)
-        skeleton_stack_x[:, :, x] = skeletonize(edge_stack_x[:, :, x])
+    s = conductor.transpose(2, 1, 0)
+    conductor_x = generate_mask_stack(s, num_worker)
+    conductor_x = conductor_x.transpose(2, 1, 0)
 
-    axes[1].imshow(blur_stack_z[d//2, :, :], cmap='gray')
-    axes[6].imshow(blur_stack_x[:, :, w//2], cmap='gray')
-
-    # mask
-    mask_stack_z = generate_mask_stack(skeleton_stack_z)
-    # mask_stack_z[:, :110//factor, :] = 0
-
-    s = skeleton_stack_x.transpose(2, 1, 0)
-    mask_stack_x = generate_mask_stack(s)
-    mask_stack_x = mask_stack_x.transpose(2, 1, 0)
-    # mask_stack_x[:, :110//factor, :] = 0
-
-    axes[2].imshow(mask_stack_z[d//2, :, :], cmap="nipy_spectral", origin="upper")
-    axes[7].imshow(mask_stack_x[:, :, w//2], cmap="nipy_spectral", origin="upper")
+    axes[2].set_title("Discrete Conductor Z")
+    axes[3].set_title("Discrete Conductor X")
+    axes[2].imshow(conductor_z[d//2, :, :], cmap="nipy_spectral", origin="upper")
+    axes[3].imshow(conductor_x[:, :, w//2], cmap="nipy_spectral", origin="upper")
 
     # potential (init)
-    potential_stack = np.zeros_like(mask_stack_z, dtype=float)
-    for y in range(h): potential_stack[:, y, :] = (1 - (y / h)) * 255
-    potential_stack[:, :1, :] = 255
-    potential_stack[:, -1:, :] = 0
+    potential = np.zeros_like(conductor_z, dtype=float)
+    for y in range(h): potential[:, y, :] = (1 - (y / h)) * 255
 
-    boundary_mask_stack = np.zeros_like(mask_stack_z, dtype=bool)
-    boundary_mask_stack[:, :1, :] = True
-    boundary_mask_stack[:, -1:, :] = True
+    # rescale again
+    potential = down_sampling(potential, rescale)
+    electrode = down_sampling(electrode, rescale, False)
+    conductor_x = down_sampling(conductor_x, rescale, False)
+    conductor_z = down_sampling(conductor_z, rescale, False)
+
+    d, h, w = potential.shape
 
     # update potential
-    flatten_stack = np.zeros((d0, h0, w0), dtype=np.uint8)
     colors = ['#000000', '#ffffff'] * 20
     cmap = ListedColormap(colors)
 
-    counts_stack_z = np.zeros((d, 256), dtype=int)
+    counts_z = np.zeros((d, 256), dtype=int)
     for z in range(d):
-        counts_stack_z[z] = np.bincount(mask_stack_z[z, :, :].flatten(), minlength=256)
+        counts_z[z] = np.bincount(conductor_z[z, :, :].flatten(), minlength=256)
 
-    counts_stack_x = np.zeros((w, 256), dtype=int)
+    counts_x = np.zeros((w, 256), dtype=int)
     for x in range(w):
-        counts_stack_x[x] = np.bincount(mask_stack_x[:, :, x].flatten(), minlength=256)
-
-    # potential_stack = tifffile.imread("potential_.tif").astype(float)
-    nrrd.write("mask_template.nrrd", np.zeros((d0, h0, w0), dtype=np.uint8))
-    top_level, bot_level = 180, 100
+        counts_x[x] = np.bincount(conductor_x[:, :, x].flatten(), minlength=256)
 
     plt.ion()
     for i in range(3000):
-        potential_stack[boundary == top_label] = top_level
-        potential_stack[boundary == bot_label] = bot_level
+        potential[electrode == top_electrode_label] = top_electrode_level
+        potential[electrode == bot_electrode_label] = bot_electrode_level
 
-        pc = update_potential_stack(potential_stack)
-        # pc[boundary_mask_stack] = potential_stack[boundary_mask_stack]
-        pc[boundary > 0] = potential_stack[boundary > 0]
-
-        # pc = update_potential_level(pc, boundary)
-        # pc[boundary > 0] = potential_stack[boundary > 0]
-
-        potential_stack = pc
+        pc = update_potential(potential)
+        pc[electrode > 0] = potential[electrode > 0]
+        potential = pc
 
         if (i%5 == 2):
-            pcx = update_mask_stack(pc, mask_stack_x, counts_stack_x, axis=2)
-            pcx[mask_stack_x <= 0] = pc[mask_stack_x <= 0]
-            # pcx[boundary_mask_stack] = pc[boundary_mask_stack]
-            pcx[boundary > 0] = pc[boundary > 0]
-            potential_stack = pcx
+            pcx = update_conductor(pc, conductor_x, counts_x, axis=2)
+            pcx[conductor_x <= 0] = pc[conductor_x <= 0]
+            pcx[electrode > 0] = pc[electrode > 0]
+            potential = pcx
 
         if (i%5 == 1):
-            pcz = update_mask_stack(pc, mask_stack_z, counts_stack_z, axis=0)
-            pcz[mask_stack_z <= 0] = pc[mask_stack_z <= 0]
-            # pcz[boundary_mask_stack] = pc[boundary_mask_stack]
-            pcz[boundary > 0] = pc[boundary > 0]
-            potential_stack = pcz
+            pcz = update_conductor(pc, conductor_z, counts_z, axis=0)
+            pcz[conductor_z <= 0] = pc[conductor_z <= 0]
+            pcz[electrode > 0] = pc[electrode > 0]
+            potential = pcz
 
         if (i%100 == 0):
             print('frame ', i)
 
-            axes[3].imshow(potential_stack[d//2, :, :], cmap=cmap)
-            axes[8].imshow(potential_stack[:, :, w//2], cmap=cmap)
-
-            if (i%500 == 0):
-            # if (i == 800 or i == 1300 or i == 1990 or i== 2990):
-            # if (i%100 == 0):
-                selected_points_top = [(x, 0) for x in range(w)]
-                selected_points_bottom = [(x, h-1) for x in range(w)]
-
-                for z in range(d0):
-                    points_array = generate_2d_points_array(potential_stack[z//factor-1], selected_points_top, selected_points_bottom, h)
-
-                    map_x = points_array[..., 0].astype(np.float32) * (w0 / w)
-                    map_y = points_array[..., 1].astype(np.float32) * (h0 / h)
-
-                    map_x = cv2.resize(map_x, (w0, h0), interpolation=cv2.INTER_LINEAR)
-                    map_y = cv2.resize(map_y, (w0, h0), interpolation=cv2.INTER_LINEAR)
-
-                    flatten_stack[z] = cv2.remap(tif_stack_origin[z], map_x, map_y, interpolation=cv2.INTER_LINEAR)
-                    flatten_stack[z, :(h0 * (255 - top_level) // 255), :] = 0
-                    flatten_stack[z, (h0 * (255 - bot_level) // 255):, :] = 0
-
-                axes[4].imshow(flatten_stack[d0//2], cmap="gray")
-                axes[9].imshow(flatten_stack[:, :, w0//2], cmap='gray')
-
-                nrrd.write("extracted_data.nrrd", flatten_stack.astype(np.uint8))
-                tifffile.imwrite("extracted_data.tif", flatten_stack.astype(np.uint8))
-                tifffile.imwrite("potential.tif", potential_stack.astype(np.uint8))
+            axes[4].set_title("Potential Slice Z")
+            axes[5].set_title("Potential Slice X")
+            axes[4].imshow(potential[d//2, :, :], cmap=cmap)
+            axes[5].imshow(potential[:, :, w//2], cmap=cmap)
 
             plt.pause(0.001)
     plt.ioff()
 
-    # show the plot
+    plt.tight_layout()
     plt.show()
 

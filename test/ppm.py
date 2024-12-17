a8ce2295fc99b62b9c75f44ea5caf15416647eed
Hsiao Yao <tomtogo1260@gmail.com>
2024-12-17 20:30:41 +0800

    dual side ppm

--- test/ppm.py
+++ test/ppm.py
@@ -12,12 +12,36 @@ def add_rect(xc, yc):
     rect = Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
     return rect
 
-if __name__ == "__main__":
-    zmin, ymin, xmin = 5049, 1765, 3380
-    zf, yf, xf = 5249, 2132, 4078
+# flatten -> original
+def generate_original_points(potential, zf, yf, xf, d, h, w):
+    # uint8 run faster
+    coords_z, coords_y, coords_x = np.indices((256, 256, 256), dtype=np.uint8)
 
-    zf, yf, xf = zf - zmin, yf - ymin, xf - xmin
-    # zf, yf, xf = 200, 367, 698
+    flat_x = update_flatten(coords_x, potential)
+    flat_y = update_flatten(coords_y, potential)
+
+    # flat_coords = np.stack((coords_z, coords_y, coords_x), axis=-1)
+    flat_coords = np.stack((coords_z, flat_y, flat_x), axis=-1)
+    zo, yo, xo = flat_coords[zf * 256//d][yf * 256//h][xf * 256//w]
+    zo, yo, xo = zo * d//256, yo * h//256, xo * w//256
+
+    return zo, yo, xo
+
+# original -> flatten
+def generate_flatten_points(potential, zo, yo, xo, d, h, w):
+    dp, hp, wp = potential.shape
+
+    yf = potential[zo * dp//d][yo * hp//h][xo * wp//w]
+
+    zf, yf, xf = zo, yf * h//256, xo
+
+    return zf, yf, xf
+
+if __name__ == "__main__":
+    zmin, ymin, xmin = (5049, 1765, 3380)
+
+    # (zo, yo, xo), (zf, yf, xf) = (None, None, None), (5249, 2132, 4078)
+    (zo, yo, xo), (zf, yf, xf) = (5247, 2182, 4076), (None, None, None)
 
     dirname = f'/Users/yao/Desktop/full-scrolls/community-uploads/yao/scroll1/{zmin:05}_{ymin:05}_{xmin:05}/'
 
@@ -32,23 +56,23 @@ if __name__ == "__main__":
     if not os.path.exists(potential_dir):
         sys.exit(f'potential {os.path.basename(potential_dir)} does not exist')
 
+    if zf is not None:
+        zf, yf, xf = zf - zmin, yf - ymin, xf - xmin
+    if zo is not None:
+        zo, yo, xo = zo - zmin, yo - ymin, xo - xmin
+    if zf is None and zo is None:
+        sys.exit(f'Please enter the coordinates (zf, yf, xf) or (zo, yo, xo).')
+
     ### load data
     volume, header = nrrd.read(volume_dir)
     flatten, header = nrrd.read(flatten_dir)
     potential, header = nrrd.read(potential_dir)
-
     d, h, w = volume.shape
 
-    # uint8 run faster
-    coords_z, coords_y, coords_x = np.indices((256, 256, 256), dtype=np.uint8)
-
-    flat_x = update_flatten(coords_x, potential)
-    flat_y = update_flatten(coords_y, potential)
-
-    # flat_coords = np.stack((coords_z, coords_y, coords_x), axis=-1)
-    flat_coords = np.stack((coords_z, flat_y, flat_x), axis=-1)
-    zo, yo, xo = flat_coords[zf * 256//d][yf * 256//h][xf * 256//w]
-    zo, yo, xo = zo * d//256, yo * h//256, xo * w//256
+    if zo is None:
+        zo, yo, xo = generate_original_points(potential, zf, yf, xf, d, h, w)
+    if zf is None:
+        zf, yf, xf = generate_flatten_points(potential, zo, yo, xo, d, h, w)
 
     print(f'original (z, y, x): {zmin+zo} {ymin+yo} {xmin+xo}')
     print(f'flatten (z, y, x): {zmin+zf} {ymin+yf} {xmin+xf}')
